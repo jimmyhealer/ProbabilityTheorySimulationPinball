09fc8b2d66657b4f5e52a942c64e714d21a9f2de
jimmyhealer <yahing6066@gmail.com>
2021-12-31 18:47:45 +0800

    add speed and optimize setting

--- build/build.iss
+++ build/build.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "機率論作業"
-#define MyAppVersion "0.1.0"
+#define MyAppVersion "0.1.1"
 #define MyAppPublisher "Jimmyhealer"
 #define MyAppURL "https://www.example.com/"
 #define MyAppExeName "main.exe"
--- main.cpp
+++ main.cpp
@@ -12,7 +12,11 @@ using namespace std;
 mt19937 gen(0x5EED);
 int randint(int lb, int ub) {
   return uniform_int_distribution<int>(lb, ub)(gen);
-  // return round(normal_distribution<>{(ub - lb) / 2, 20}(gen));
+  int ret = lb - 1;
+  while (ret < lb || ret > ub) {
+    ret = round(normal_distribution<>{(ub - lb) / 2, (ub - lb) / 6}(gen));
+  }
+  return ret;
 }
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -36,20 +40,20 @@ int main(int argv, char** args) {
   FC_Font* h2 = FC_CreateFont();
   FC_Font* h3 = FC_CreateFont();
   FC_Font* p = FC_CreateFont();
-  FC_LoadFont(font, renderer, "sources/minu.ttf", 60, FC_MakeColor(0, 200, 0, 255),
-              TTF_STYLE_NORMAL);
-  FC_LoadFont(h2, renderer, "sources/minu.ttf", 40, FC_MakeColor(255, 255, 255, 255),
-              TTF_STYLE_NORMAL);
-  FC_LoadFont(h3, renderer, "sources/minu.ttf", 22, FC_MakeColor(255, 255, 255, 255),
-              TTF_STYLE_NORMAL);
-  FC_LoadFont(p, renderer, "sources/minu.ttf", 16, FC_MakeColor(255, 255, 255, 255),
-              TTF_STYLE_NORMAL);
+  FC_LoadFont(font, renderer, "sources/minu.ttf", 60,
+              FC_MakeColor(0, 200, 0, 255), TTF_STYLE_NORMAL);
+  FC_LoadFont(h2, renderer, "sources/minu.ttf", 40,
+              FC_MakeColor(255, 255, 255, 255), TTF_STYLE_NORMAL);
+  FC_LoadFont(h3, renderer, "sources/minu.ttf", 22,
+              FC_MakeColor(255, 255, 255, 255), TTF_STYLE_NORMAL);
+  FC_LoadFont(p, renderer, "sources/minu.ttf", 16,
+              FC_MakeColor(255, 255, 255, 255), TTF_STYLE_NORMAL);
 
   SDL_StartTextInput();
   string in;
   bool running = true;
   srand(time(0));
-  float loadingTime = rand() % 4;
+  float loadingTime = rand() % 3;
 
   while (running) {
     SDL_Event event;
@@ -60,7 +64,7 @@ int main(int argv, char** args) {
                  "指導教授: 許玉平教授");
     // FC_DrawAlign(h3, renderer, 400, 450, FC_ALIGN_CENTER, "按任意繼續");
     float time = SDL_GetTicks() / 1000.0f;
-    int rgb = (int)min(255, 255 - (int)(sin(time) * 127));
+    int rgb = (int)max(min(255, 255 - (int)(abs(sin(time * 1.2)) * 127)), 0);
     char* msg = "按任意鍵繼續";
     if (time < loadingTime) msg = "Loading...";
     FC_DrawEffect(h3, renderer, 400, 450,
@@ -99,7 +103,8 @@ int main(int argv, char** args) {
   int ran1;
   int screenshot = 0;
   float screenshot_timer = 0;
-  float height_interval = 5.0;
+  float height_interval = 40.0;
+  int speed = 0;
   while (running) {
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
@@ -108,17 +113,33 @@ int main(int argv, char** args) {
         if (event.key.keysym.sym == SDLK_r) {
           memset(height, 0, sizeof(height));
           cnt = 0;
+          con = 0;
+          speed = 0;
+          timer = 30;
+          height_interval = 40.0;
         } else if (event.key.keysym.sym == SDLK_KP_PLUS) {
-          timer = timer - 10 < 0 ? 0 : timer - 10;
+          if (timer)
+            timer = timer - 10 < 0 ? 0 : timer - 10;
+          else if(speed < 1000)
+            speed = min(10000, speed + 10);
+          else if(speed < 10000)
+            speed = min(10000, speed + 100);
         } else if (event.key.keysym.sym == SDLK_KP_MINUS) {
-          timer = timer + 10 > 100 ? 100 : timer + 10;
+          if (speed == 0)
+            timer = timer + 10 > 300 ? 300 : timer + 10;
+          else
+            speed = max(0, speed - 10);
+        } else if (event.key.keysym.sym == SDLK_0 || event.key.keysym.sym == SDLK_KP_0) {
+          timer = 30;
+          speed = 0;
         } else if (event.key.keysym.sym == SDLK_q) {
           return 0;
         } else if (event.key.keysym.sym == SDLK_p) {
           con = !con;
         } else if (event.key.keysym.sym == SDLK_h) {
           help = !help;
-        } else if (event.key.keysym.sym == SDLK_s && SDL_GetTicks() - screenshot_timer > 1000) {
+        } else if (event.key.keysym.sym == SDLK_s &&
+                   SDL_GetTicks() - screenshot_timer > 1000) {
           std::time_t result = std::time(nullptr);
           SDL_Surface* sshot =
               SDL_CreateRGBSurface(0, WIDTH, HEIGHT, 32, 0x00ff0000, 0x0000ff00,
@@ -135,45 +156,48 @@ int main(int argv, char** args) {
         }
       }
     }
-    if(screenshot) {
-      if(SDL_GetTicks() - screenshot_timer > 2000) {
+    if (screenshot) {
+      if (SDL_GetTicks() - screenshot_timer > 2000) {
         screenshot = 0;
       }
     }
     SDL_SetRenderDrawColor(renderer, 32, 42, 67, 255);
     SDL_RenderClear(renderer);
     FC_DrawAlign(h3, renderer, 10, 30, FC_ALIGN_LEFT, "目前數量為: %d", cnt);
-    FC_DrawAlign(p, renderer, 100, 550, FC_ALIGN_CENTER, "%d", 0);
-    FC_DrawAlign(p, renderer, 400, 550, FC_ALIGN_CENTER, "%d", 50);
-    FC_DrawAlign(p, renderer, 700, 550, FC_ALIGN_CENTER, "%d", 100);
+    FC_DrawAlign(p, renderer, 100, 550, FC_ALIGN_CENTER, "%d", -1);
+    FC_DrawAlign(p, renderer, 400, 550, FC_ALIGN_CENTER, "%d", 0);
+    FC_DrawAlign(p, renderer, 700, 550, FC_ALIGN_CENTER, "%d", 1);
     FC_DrawAlign(p, renderer, 80, 535, FC_ALIGN_CENTER, "%d", 0);
-    FC_DrawAlign(p, renderer, 80, 150, FC_ALIGN_CENTER, "%d", (int)(400.0 / height_interval));
-    FC_DrawAlign(p, renderer, 80, 342, FC_ALIGN_CENTER, "%d", (int)(400.0 / height_interval) / 2);
+    FC_DrawAlign(p, renderer, 80, 150, FC_ALIGN_CENTER, "%d",
+                 (int)(400.0 / height_interval));
+    FC_DrawAlign(p, renderer, 80, 342, FC_ALIGN_CENTER, "%d",
+                 (int)(400.0 / height_interval) / 2);
     FC_DrawAlign(h3, renderer, 640, 30, FC_ALIGN_RIGHT, "%s",
                  screenshot ? "已儲存截圖" : "");
     FC_DrawAlign(h3, renderer, 650, 30, FC_ALIGN_LEFT, "%s",
                  help ? "按 h 關閉幫助" : "按 h 開啟幫助");
     FC_DrawAlign(h3, renderer, 650, 60, FC_ALIGN_LEFT, "%s",
                  help ? "按 q 關閉程式\n按 p 暫停模擬\n按 r 重新模擬\n按 + "
-                        "加速模擬\n按 - 減速模擬\n按 s 儲存截圖"
+                        "加速模擬\n按 - 減速模擬\n按 0 正常速度\n按 s 儲存截圖"
                       : "");
     SDL_SetRenderDrawColor(renderer, 232, 171, 208, 0);
     FC_DrawAlign(h3, renderer, 10, 60, FC_ALIGN_LEFT, "%s",
                  con ? "暫停中" : "模擬中");
+    FC_DrawAlign(h3, renderer, 12, 90, FC_ALIGN_LEFT, "%.2f 倍速",
+                 timer ? 30.0 / timer : max(4.0f, float(speed)));
 
     if (!con) {
-      ++cnt;
-      ran1 = (sqrt(2500 - randint(0, INT_MAX) % 2501));
-      if (randint(0, INT_MAX) % 2) {
-        ran1 = 99 - ran1;
+      int flag = 1;
+      if (speed) flag = speed + (rand() % (speed / 8 + 1)) - (rand() % (speed / 8 + 1));
+      for (int i = 0; i < flag; ++i) {
+        ++cnt;
+        ran1 = (sqrt(2500 - randint(0, INT_MAX) % 2501));
+        if (randint(0, INT_MAX) % 2) {
+          ran1 = 99 - ran1;
+        }
+        height[ran1]++;
+        // height[randint(0, 100)]++;
       }
-      height[ran1]++;
-      // int b = -1;
-      // while(b < 0 || b > 100){
-      //   b = randint(0, 100);
-      // }
-      // SDL_Log("%d", b);
-      // height[b]++;
     }
     int max_height = 0;
     for (int i = 0; i < 102; ++i) {
